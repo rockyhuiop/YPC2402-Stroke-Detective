3de7340c9524df6fa76d71b746218023b5571d91
24asx4 <24asasasas@gmail.com>
2024-11-29 04:15:34 +0800

    update collider after changing pose

--- Assets/YPC2402/script/PoseControl.cs
+++ Assets/YPC2402/script/PoseControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PoseControl : MonoBehaviour
 {
     public Animator PoseAnimator;
+    //current pose
     [SerializeField] string CurrnetState;
     // Start is called before the first frame update
     void Start()
@@ -18,9 +19,12 @@ public class PoseControl : MonoBehaviour
         if ( PoseAnimator == null ) {
             PoseAnimator = GetComponent<Animator>();
         }
-        //set the pose
+        //if current state not set
         if (CurrnetState!=""&&!PoseAnimator.GetCurrentAnimatorStateInfo(0).IsName(CurrnetState)) {
+            //set the pose
             PoseAnimator.Play(CurrnetState);
+            //update the collider (collider need to update manually if pose changed)
+            GetComponent<SkinnedCollider>().UpdateCollider();
         }
     }
 }
