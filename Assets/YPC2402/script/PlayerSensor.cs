2a7886245cd4c0abe880501c6def7ac02c5e9276
24asx4 <24asasasas@gmail.com>
2025-03-27 23:22:27 +0800

    improve NPC turn and face player

--- Assets/YPC2402/script/PlayerSensor.cs
+++ Assets/YPC2402/script/PlayerSensor.cs
@@ -38,17 +38,45 @@ public class PlayerSensor : MonoBehaviour
             if (collider.transform.CompareTag("Player")) {
                 player = collider.transform;
                 NPC = GetComponent<Transform>().parent;
-                Debug.Log("player: "+collider.transform.rotation.eulerAngles.y+" NPC: "+GetComponent<Transform>().parent.rotation.eulerAngles.y+" diff: "+math.abs(player.rotation.eulerAngles.y - NPC.rotation.eulerAngles.y-180) );
                 havePlayer=true;
                 break;
             }          
         }
+        //stop NPC nav ai moving
         navAI.force_stop=havePlayer;
+        //turn and face player
         if (havePlayer) {
-            if ((player.rotation.eulerAngles.y - NPC.rotation.eulerAngles.y-180)>0.5){
-                NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y-5.0f * Time.deltaTime,NPC.eulerAngles.z);
-            } else if ((player.rotation.eulerAngles.y - NPC.rotation.eulerAngles.y - 180) < 0.5){
-                NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y+5.0f * Time.deltaTime,NPC.eulerAngles.z);
+            float NPC_rot_adjust=NPC.rotation.eulerAngles.y%360;
+            if (NPC_rot_adjust < 0) {
+                NPC_rot_adjust=360-NPC_rot_adjust;
+            }
+            float target_deg=(Mathf.Atan(math.abs(player.transform.position.z-NPC.transform.position.z)/math.abs(player.transform.position.x-NPC.transform.position.x))*Mathf.Rad2Deg)%360;
+            if (player.transform.position.z>NPC.transform.position.z && player.transform.position.x>NPC.transform.position.x) {
+                target_deg=90-target_deg;
+            } else if (player.transform.position.z<NPC.transform.position.z && player.transform.position.x>NPC.transform.position.x) {
+                target_deg=90+target_deg;
+            } else if (player.transform.position.z<NPC.transform.position.z && player.transform.position.x<NPC.transform.position.x) {
+                target_deg=270-target_deg;
+            } else if (player.transform.position.z>NPC.transform.position.z && player.transform.position.x<NPC.transform.position.x) {
+                target_deg=270+target_deg;
+            }
+            //Debug.Log("player: "+player.transform.position.z+" " + player.transform.position.x +" NPC: "+NPC.position.z +" "+NPC.position.x+" diff: "+(target_deg)+" "+NPC.eulerAngles.y );
+            float speed=60.0f;
+            if (math.abs(NPC_rot_adjust-target_deg)>speed*0.8 * Time.deltaTime){
+                if (NPC_rot_adjust>target_deg){
+                    if (NPC_rot_adjust - target_deg < 180) {
+                        NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y-speed * Time.deltaTime,NPC.eulerAngles.z);
+                    } else {
+                        NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y+speed * Time.deltaTime,NPC.eulerAngles.z);
+                    }
+                } else {
+                    if (target_deg - NPC_rot_adjust > 180) {
+                        NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y-speed * Time.deltaTime,NPC.eulerAngles.z);
+                    } else {
+                        NPC.eulerAngles = new Vector3(NPC.eulerAngles.x,NPC.eulerAngles.y+speed * Time.deltaTime,NPC.eulerAngles.z);
+                    }
+                    
+                }
             }
         }
     }
