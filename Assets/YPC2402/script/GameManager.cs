61376dc9058d616654dd4fb33df0f2c0b5eb63b6
24asx4 <24asasasas@gmail.com>
2025-04-07 03:20:31 +0800

    rough try of send and get data from db

--- Assets/YPC2402/script/GameManager.cs
+++ Assets/YPC2402/script/GameManager.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -15,9 +18,11 @@ public class GameManager : MonoBehaviour
     public int Wrong_NPC=0;
     public TMP_Text Scoreboard;
     public GameObject Finish;
+    private float avg_acc;
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(UploadResult(0));
         if(instance == null){
             instance = this;
         }else{
@@ -40,7 +45,7 @@ public class GameManager : MonoBehaviour
     private void InitializeDictionary(){
         for (int i = 0; i < NPCs.Length; i++)
         {
-            NPCDataDict.Add(NPCs[i], NPCData[Random.Range(0,NPCData.Length-1)]);
+            NPCDataDict.Add(NPCs[i], NPCData[UnityEngine.Random.Range(0,NPCData.Length-1)]);
         }
     }
 
@@ -83,11 +88,64 @@ public class GameManager : MonoBehaviour
         Finish.transform.GetChild(0).GetComponent<TMP_Text>().SetText(
             "Congratulations!\n" +
             "You have finished the game!\n" +
-            "The correct rate is "+ Correct_NPC*100.0/NPCs.Length + "%");
+            "The correct rate is "+ Correct_NPC*100.0/NPCs.Length + "%\n"+
+            "The average correct rate is "+ avg_acc
+        );
+        StartCoroutine(UploadResult(Correct_NPC*100.0/NPCs.Length));
     }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    IEnumerator UploadResult(double acc)
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(
+            "http://localhost:5000/api/level1/add_result", 
+            $"{{ \"NPC\": [1, 0, 1], \"acc\": {acc.ToString()}, \"time\": 56.5 }}",
+            "application/json"
+            ))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+        String uri="http://localhost:5000/api/level1/get_avg";
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    String result=webRequest.downloadHandler.text;
+                    result=result.Replace("{", "");
+                    result=result.Replace("}", "");
+                    result=result.Split(":")[1].Trim();
+                    avg_acc = float.Parse(result);
+                    Debug.Log(pages[page] + ":\nReceived: " + result);
+                    break;
+            }
+        }
     }
 }
